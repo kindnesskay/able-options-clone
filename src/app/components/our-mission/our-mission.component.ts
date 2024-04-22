436d7a052a7790c588495f2450ecce1a65c0f0c7
kindesskay <kindenessudubamo@gmail.com>
2024-04-22 07:26:22 +0100

    ui changes

--- src/app/components/our-mission/our-mission.component.ts
+++ src/app/components/our-mission/our-mission.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-our-mission',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './our-mission.component.css'
 })
 export class OurMissionComponent {
-
+  @Input() image!: string;
 }
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ImageSideComponent } from '../components/image-side/image-side.component';
+import { ServiceCardComponent } from '../components/service-card/service-card.component';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [ImageSideComponent],
+  imports: [ImageSideComponent,ServiceCardComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
