d349fb78ac23e526274bef08455b961ea6fd45c5
kindesskay <kindenessudubamo@gmail.com>
2024-04-22 08:54:00 +0100

    quick fix

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,14 +9,36 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  isMenuOpen = false;
+  constructor(private router: Router) {
+    // Subscribe to router events
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Close menu whenever route changes
+        this.close()
+        // Set title based on route or any logic you need
+      }
+    });
+  }
+
   toggleMenu() {
-    const menu = document.getElementById("menu");
-    menu?.classList.toggle("show")
-    const blur_box=document.querySelector('#blur-box')
-    blur_box?.classList.toggle('blur-box')
-    blur_box?.classList.toggle('hide')
-    
-    
+    const menu = document.getElementById('menu');
+
+    menu?.classList.toggle('show');
+    const blur_box = document.querySelector('#blur-box');
+    blur_box?.classList.toggle('blur-box');
+    blur_box?.classList.toggle('hide');
+
     // const menu_btn = document.querySelector('.menu-btn');
+  }
+
+  close(){
+    const menu = document.getElementById('menu');
+
+    menu?.classList.add('show');
+    const blur_box = document.querySelector('#blur-box');
+    blur_box?.classList.remove('blur-box');
+    blur_box?.classList.add('hide');
+
   }
 }
