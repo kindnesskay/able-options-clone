94bff84cb1e4fe27b204f2ffaff8fde772119282
kindesskay <kindenessudubamo@gmail.com>
2024-04-23 09:24:36 +0100

    added navbar animation on scroll

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { ScrollService } from '../scroll.service';
 
 @Component({
   selector: 'app-header',
@@ -10,20 +11,32 @@ import { NavigationEnd, Router, RouterLink } from '@angular/router';
 })
 export class HeaderComponent {
   isMenuOpen = false;
-  constructor(private router: Router) {
+  menuIcon = 'assets/menu.svg';
+  constructor(private router: Router,scrollService:ScrollService) {
     // Subscribe to router events
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         // Close menu whenever route changes
-        this.close()
+        this.close();
+        scrollService.scrollToTop()
         // Set title based on route or any logic you need
       }
     });
   }
 
+  toggleIcon() {
+    if (!this.isMenuOpen) {
+      this.menuIcon = 'assets/menu.svg';
+      return;
+    }
+    this.menuIcon = 'assets/close.svg';
+    return;
+  }
   toggleMenu() {
     const menu = document.getElementById('menu');
-
+    this.isMenuOpen = !this.isMenuOpen;
+    // change menu icon
+    this.toggleIcon();
     menu?.classList.toggle('show');
     const blur_box = document.querySelector('#blur-box');
     blur_box?.classList.toggle('blur-box');
@@ -32,13 +45,20 @@ export class HeaderComponent {
     // const menu_btn = document.querySelector('.menu-btn');
   }
 
-  close(){
+  close() {
+    this.isMenuOpen=false
+    this.toggleIcon()
     const menu = document.getElementById('menu');
 
     menu?.classList.add('show');
     const blur_box = document.querySelector('#blur-box');
     blur_box?.classList.remove('blur-box');
     blur_box?.classList.add('hide');
+  }
+  isScrolled: boolean = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 0;
   }
 }
