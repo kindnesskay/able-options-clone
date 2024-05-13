e33d96bad30345bf525d367b17ea4839396ecb55
kindesskay <kindenessudubamo@gmail.com>
2024-05-13 06:10:07 +0100

    minnor changes that longer than its supposed to

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,18 @@ import { ContentThreeComponent } from '../components/home/content-three/content-
 import { OurValuesComponent } from '../components/our-values/our-values.component';
 import { OurMissionComponent } from '../components/our-mission/our-mission.component';
 import { SliderComponent } from '../components/slider/slider.component';
+import { TheteamComponent } from '../components/the-team/the-team.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [OurMissionComponent, OurValuesComponent, ContentThreeComponent,SliderComponent],
+  imports: [
+    OurMissionComponent,
+    OurValuesComponent,
+    ContentThreeComponent,
+    SliderComponent,
+    TheteamComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
