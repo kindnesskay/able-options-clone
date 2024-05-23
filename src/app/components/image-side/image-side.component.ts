010cbb3fa3a68216a1299e031b2a7de9ec83f51a
kindesskay <kindenessudubamo@gmail.com>
2024-05-23 11:37:02 +0100

    new images

--- src/app/components/image-side/image-side.component.ts
+++ src/app/components/image-side/image-side.component.ts
@@ -4,42 +4,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-image-side',
   standalone: true,
-  imports: [NgFor, NgStyle],
+  imports: [NgStyle],
   templateUrl: './image-side.component.html',
   styleUrl: './image-side.component.css',
 })
 export class ImageSideComponent {
   @Input() title!: string;
   @Input() image!: string;
-
-  // [
-  //   'assets/boy.jpg',
-  //   'assets/people-with-disabilities.jpg',
-  //   'assets/baking.webp',
-  //   'assets/girl-on-wheelchair.webp',
-  // ]; // Add your image URLs here
-  // currentIndex: number = -1;
-  // interval:any
-
-  // nextSlide() {
-  //   if (this.currentIndex == this.images.length - 1) {
-  //     this.currentIndex = -1;
-  //   }
-  //   this.currentIndex++;
-  // }
-
-  // startSlideShow():void{
-  //   this.interval=setInterval(()=>{
-  //     this.nextSlide()
-  //   },3000)
-  //   this.nextSlide()
-
-  // }
-  // ngOnInit():void{
-  //   this.startSlideShow()
-  // }
-
-  // ngOnDestroy(){
-  //   clearInterval(this.interval)
-  // }
 }
